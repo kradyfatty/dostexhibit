0c76d46015f67253a07c2eabd6e8586225c93111
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-11-16 10:59:15 +0800

    Create index.php

---
+++ coundown/index.php
@@ -0,0 +1,33 @@
+<?php require_once("includes/header.php"); ?>
+    <body>
+        <div class="loader-wrap">
+            <div class="pin"></div>
+            <div class="pulse"></div>
+        </div>
+        <div id="main">
+            <?php require_once("includes/navigation.php");?>
+            
+        
+            <div class="content full-height fs-slider-wrap">
+                <section class="hero-section no-dadding full-height"  id="sec1">
+                    <div class="slider-container-wrap full-height fs-slider fl-wrap">
+                        <div class="slider-container">
+                            <div class="slider-item fl-wrap">
+                                <div class="bg" id="bg" data-bg="images/bg/dost3.jpg"></div>
+                                <div class="overlay"></div>
+                                <div class="hero-section-wrap fl-wrap">
+                                    
+                                
+                                    <div id="container" >
+                                        
+                                    </div>
+                                    
+                                </div> 
+                            </div>
+                            <div class="bubble-bg"> </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            
+        <?php require_once("includes/footer.php"); ?>
