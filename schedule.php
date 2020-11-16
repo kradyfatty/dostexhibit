bb06f13231735c91476031ed9da387549ee2675d
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2020-11-16 17:07:13 +0800

    update

---
+++ schedule.php
@@ -0,0 +1,30 @@
+<?php require_once("includes/header.php"); ?>
+    <body>
+        <embed src="/audio/mixkit-feeling-happy-5.mp3" loop="true" autostart="true" width="2" height="0">
+        <div class="loader-wrap">
+            <div class="pin"></div>
+            <div class="pulse"></div>
+        </div>
+        <div id="main">
+            <?php require_once("includes/navigation.php");?>
+            
+        
+            <div id="wrapper">
+                <div class="content">
+
+                    <section>
+                        <div class="container">
+                            <div class="about-wrap">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <img src="images/banners/schedule.png">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+            </div>
+            
+        <?php require_once("includes/footer.php"); ?>
