ad1dac5cbc74f1789140082141b7b2f683e82afb
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2020-11-18 16:58:05 +0800

    Kinabukasan Updated
    :)

--- kinabukasan.php
+++ kinabukasan.php
@@ -20,16 +20,18 @@
 
                                     <img id="k6k12" src="images/header/kinabukasan/floor.png" alt="6" />
                                     <img id="k6k1" rel="3box" src="images/header/kinabukasan/3box.png" alt="6" />
-                                    <img id="k6k2" rel="stand2" src="images/header/kinabukasan/stand2.png" alt="6" />
-                                    <img id="k6k3" rel="box1" src="images/header/kinabukasan/box.png" alt="6" />
-                                    <img id="k6k4" rel="box2" src="images/header/kinabukasan/box.png" alt="6" />
-                                    <img id="k6k5" rel="box3" src="images/header/kinabukasan/box.png" alt="6" />
+                                    <img id="k6k3" rel="box1" src="images/header/kinabukasan/box4.png" alt="6" />
+                                    <img id="k6k4" rel="box2" src="images/header/kinabukasan/box3.png" alt="6" />
+                                    <img id="k6k5" rel="box3" src="images/header/kinabukasan/box2.png" alt="6" />
+                                    <img id="k6k55" rel="box3" src="images/header/kinabukasan/box1.png" alt="6" />
                                     <img id="k6k6" rel="table" src="images/header/kinabukasan/table.png" alt="6" />
                                     <img id="k6k7" rel="girl" src="images/header/kinabukasan/girl.png" alt="6" />
                                     <img id="k6k8" rel="red" src="images/header/kinabukasan/red.png" alt="6" />
                                     <img id="k6k9" rel="stand" src="images/header/kinabukasan/stand.png" alt="6" />
                                     <img id="k6k10" rel="boy" src="images/header/kinabukasan/boy.png" alt="6" />
-                                    <img id="k6k11" rel="girlred" src="images/header/kinabukasan/girlred.png" alt="6" />
+                                    <img id="k6k14" rel="red" src="images/header/kinabukasan/chairs.png" alt="6" />
+                                    <img id="k6k15" rel="red" src="images/header/kinabukasan/wallpaper.png" alt="6" />
+                                    <img id="k6k16" rel="red" src="images/header/kinabukasan/wallpaper2.png" alt="6" />
                                 </div>
                             </div>           
                         </div>
