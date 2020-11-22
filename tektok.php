5762e6f00b56ebdd3d05c81f53e7247c45c7f969
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2020-11-22 23:16:20 +0800

    Updated

---
+++ tektok.php
@@ -0,0 +1,32 @@
+<?php require_once("includes/header.php"); ?>
+    <body>
+        <iframe src="audio/mixkit-feeling-happy-5.mp3" allow="autoplay" style="display:none" id="iframeAudio"></iframe> 
+        <div class="loader-wrap">
+            <div class="pin"></div>
+            <div class="pulse"></div>
+        </div>
+        <div id="main">
+            <?php require_once("includes/navigation.php");?>
+            
+            <div class="content full-height fs-slider-wrap">
+                <section class="color-bg full-height">
+                    <!-- <img src="images/banners/left.png" style="top: 0; left: 0; position: absolute; width: auto; height: 80%;">
+                    <img src="images/banners/right.png" style="bottom: 0; right: 0; position: absolute; width: auto; height: 50%;"> -->
+                    <div class="shapes-bg-big1"></div>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                
+                             <img src="images/countdown/tektok.png" style="margin-top: -40px; width: auto; height: calc(100vh - 180px);">
+                            
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bubble-bg"> </div>
+                    <!-- <div class="header-sec-link">
+                        <div class="container"><a href="" class="custom-scroll-link">Let's Start</a></div>
+                    </div> -->
+                </section>
+            </div>
+            
+        <?php require_once("includes/footer.php"); ?>
