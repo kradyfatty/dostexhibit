fec7d84a0745ef1bf64a598c65174b88fdf8c01d
kradyfatty <67622951+kradyfatty@users.noreply.github.com>
2020-11-23 15:10:51 +0800

    Video Updated

--- kabuhayan/kinglandhopia.php
+++ kabuhayan/kinglandhopia.php
@@ -15,8 +15,13 @@
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="video-box fl-wrap">
-                                            <img src="../images/all/sample.jpg" alt="">
-                                            <a class="video-box-btn image-popup" href="https://www.youtube.com/watch?v=Aq_Hw_N4mYU"><i class="fa fa-play" aria-hidden="true"></i></a>
+                                            <div class="list-single-main-item fl-wrap" id="sec3">
+                                                <div class="iframe-holder fl-wrap">
+                                                    <div class="resp-video">
+                                                        <iframe src="kingland/avp.mp4" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen="" frameborder="0"></iframe>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
